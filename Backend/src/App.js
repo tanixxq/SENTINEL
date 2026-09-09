b3f648ab294a3630827d5ed1191b22c47300775e
tanishq singhal <ts2311180@gmail.com>
2026-09-09 15:52:04 +0530

    feat : add monitor crud api

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import MonitorRoutes from "./Routes/MonitorRoutes.js";
+
+
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/monitors", MonitorRoutes);
 
 app.get("/",(req,res)=>{
     res.json({
