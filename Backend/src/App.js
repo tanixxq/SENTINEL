f08d9324386b6a005d58102c83d48edcd7e00fed
tanishq singhal <ts2311180@gmail.com>
2026-09-15 11:01:45 +0530

    Add JWT authentication and monitor ownership

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,21 +1,39 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import MonitorRoutes from "./Routes/MonitorRoutes.js";
-
-
+import AuthRoutes from "./Routes/AuthRoutes.js";
+import authMiddleware from "./Middleware/authMiddleware.js";
 
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
+
+app.use("/auth", AuthRoutes);
+
 app.use("/monitors", MonitorRoutes);
 
-app.get("/",(req,res)=>{
+
+app.get("/protected-test", authMiddleware, (req, res) => {
+
     res.json({
-        message:"Sentinel Backend is running"
+        message: "You accessed a protected route",
+        user: req.user
     });
-})
+
+});
+
+
+app.get("/", (req, res) => {
+
+    res.json({
+        message: "Sentinel Backend is running"
+    });
+
+});
+
 
 export default app;
